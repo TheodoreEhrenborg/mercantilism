9e3529ccff274cf031a5798b5205edfe335ec6db
TheodoreEhrenborg <46494248+TheodoreEhrenborg@users.noreply.github.com>
2019-01-09 11:14:59 -0500

    Wrote list of options

--- human_friendly.py
+++ human_friendly.py
@@ -1,3 +1,20 @@
 '''This module manages the interaction with the user and
 sends the user's commands to a file read by the artificial
 primary investigator.'''
+def main():
+  normal = '''The computer is currently running Theodore's AP Research project
+              between midnight and 2:30 pm. Type 'quit' to safely quit the program.
+              Type 'options' to get more options.'''
+  options = '''Type 'quit' to safely quit the program.
+              Type 'options' to get this options message.
+              Type 'adjourn' to get the program to cease operations until midnight.
+              Type 'reload' to tell the Artificial Primary Investigator (API) that conditions may have changed.
+              Type 'reset algorithm_name all' to get the API to reset all the trials where every algorithm was
+                  tested to see if it is ES against algorithm_name.
+              Type 'reset all algorithm_name' to get the API to reset all the trials where algorithm_name was
+                  tested to see if it is ES against all other algorithms.
+              Type 'reset all all' to get the API to reset ALL the trials. This is reversible, but you have to 
+                  know how API thinks. Be careful.
+              Type 'reset algorithm_name1 algorithm_name2' to get the API to reset all the trials where 
+                  algorithm_name1 was tested to see if it is evolutionary stable (ES) against algorithm_name2.'''
+  
