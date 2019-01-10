3fd02fef5b7223aa202888f0b1d7ad7a3bd62819
TheodoreEhrenborg <theodore.ehrenborg@gmail.com>
2019-01-10 17:25:23 -0500

    Removed midnight_watchdog (unrelated to topic)

--- midnight_watchdog.py
+++
@@ -1,4 +0,0 @@
-'''At 11:55 pm, this program quits mprime, Firefox, Chrome, Safari, and Mathematica.
-human_friendly.py does not start this program; it must be started independently.'''
-def main():
-    pass
