ebb5916720b930f2422601b00bcb92c5b3b3631b
TheodoreEhrenborg <46494248+TheodoreEhrenborg@users.noreply.github.com>
2019-01-18 11:09:29 -0500

    Started play_highest()

--- algorithms.py
+++ algorithms.py
@@ -1,2 +1,6 @@
 '''This file contains the various algorithms that play the game.
 Methods starting with aux_ are not playing the game.'''
+def play_highest():
+    '''Always play the highest token left.'''
+    return max( )
+  
