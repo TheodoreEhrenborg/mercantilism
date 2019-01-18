c628e5afff346019c41a0f97da3ff3a372238400
TheodoreEhrenborg <46494248+TheodoreEhrenborg@users.noreply.github.com>
2019-01-18 15:06:04 -0500

    Wrote aux_power, aux_exp, & algorithms using them

--- algorithms.py
+++ algorithms.py
@@ -1,21 +1,75 @@
 '''This file contains the various algorithms that play the game.
 Methods starting with aux_ are not playing the game.'''
 def play_highest(tokens, data, game_name):
-    '''Always play the highest token left.'''
+    '''Plays the highest token left.'''
     import time
-    f = open("Results/play_highest.log")
+    f = open("Results/play_highest.log","a")
     f.write(time.asctime() + ": Got message from Game " + game_name + ". Tokens received " + str(tokens) + ". Data received " + str(data)." + "\n")
     choice = max( tokens )
     f.write(time.asctime() + ": Chose " + str(choice) + "\n")
     f.close()
     return choice
 def uniform(tokens, data, game_name):
-    '''Always play a token randomly chosen based on a uniform distribution.'''
+    '''Plays a token randomly, chosen based on a uniform distribution.'''
     import time, random
-    f = open("Results/uniform.log")
+    f = open("Results/uniform.log","a")
     f.write(time.asctime() + ": Got message from Game " + game_name + ". Tokens received " + str(tokens) + ". Data received " + str(data)." + "\n")
     choice = random.choice( tokens )
     f.write(time.asctime() + ": Chose " + str(choice) + "\n")
     f.close()
     return choice
-  
+def aux_exp(tokens, data, game_name, n):
+    '''Plays a token based on a distribution where each token is chosen with 
+    weight proportional to n^(value) '''
+    import time, random
+    f = open("Results/exp.log","a")
+    f.write(time.asctime() + ": Got message from Game " + game_name + ". Tokens received " + str(tokens) + ". Data received " + str(data) +  ". n = "  + str(n) + "\n")
+    sum = 0
+    for x in tokens:
+        sum += n**x
+    random_choice = random.random() * sum
+    for x in tokens:
+        random_choice -= n**x
+        if random_choice < 0:
+            break
+    else:
+        raise Exception("Uh-oh. The program should never reach this step.")
+    choice = x
+    f.write(time.asctime() + ": Chose " + str(choice) + "\n")
+    f.close()
+    return choice
+def exp_2(tokens, data, game_name):
+    '''Plays a token based on a distribution where each token is chosen with 
+    weight proportional to 2^(value) '''
+    return aux_exp(tokens, data, game_name, 2)
+def aux_power(tokens, data, game_name, n):
+    '''Plays a token based on a distribution where each token is chosen with 
+    weight proportional to (value)^n '''
+    import time, random
+    f = open("Results/power.log","a")
+    f.write(time.asctime() + ": Got message from Game " + game_name + ". Tokens received " + str(tokens) + ". Data received " + str(data) +  ". n = "  + str(n) + "\n")
+    sum = 0
+    for x in tokens:
+        sum += x**n
+    random_choice = random.random() * sum
+    for x in tokens:
+        random_choice -= x**n
+        if random_choice < 0:
+            break
+    else:
+        raise Exception("Uh-oh. The program should never reach this step.")
+    choice = x
+    f.write(time.asctime() + ": Chose " + str(choice) + "\n")
+    f.close()
+    return choice
+def power_2(tokens, data, game_name):
+    '''Plays a token based on a distribution where each token is chosen with 
+    weight proportional to (value)^2 '''
+    return aux_power(tokens, data, game_name, 2)
+def power_1(tokens, data, game_name):
+    '''Plays a token based on a distribution where each token is chosen with 
+    weight proportional to its value'''
+    return aux_power(tokens, data, game_name, 1)
+            
+    
+      
